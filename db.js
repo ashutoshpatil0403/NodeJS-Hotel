40fc2e978b81e67ada1f57ee461ba29b59b1fa38
Ashutosh Sunil Patil <ashutoshpatil0403@gmail.com>
2024-11-27 18:10:06 +0530

    changes shift to mongoDB Cluster online server

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
-
-const mongoURL="mongodb://127.0.0.1:27017/hotels";
+require('dotenv').config()
+// const mongoURL="mongodb://127.0.0.1:27017/hotels";
+const mongoURL=process.env.CLUSTER_DB_URL;
+// const mongoURL=process.env.LOCAL_DB_URL;
 
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const db=require('./db');
+require('dotenv').config()
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());  //req.body
@@ -15,6 +16,8 @@ app.use('/person',personRoute)
 const menuRoute=require('./routes/menuRoutes')
 app.use('/menu',menuRoute)
 
-app.listen(3000,()=>{
+const PORT=process.env.PORT || 3000
+
+app.listen(PORT,()=>{
     console.log("Server is running on port 3000")
 })
